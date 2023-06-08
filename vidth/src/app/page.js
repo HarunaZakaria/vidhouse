afd6295768bc8b7784b73afb9ea487126c001754
harunaZakaria <Harunzy55@gmail.com>
2023-06-08 01:03:20 +0000

    install tailwind css

--- vidth/src/app/page.js
+++ vidth/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <div className="container">
-      <h1>Hello World</h1>
+      <h1 className='text-red-400'>Hello World</h1>
     </div>
     
     </>
